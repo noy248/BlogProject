f253a74fea2867f90a15b7d867d304177533fe11
noy248 <noya248@gmail.com>
2017-10-03 21:54:38 +0300

    Update Post.cs
    adding the display names and types

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -19,16 +19,29 @@ namespace ShaulisBlog.Models
         [Required]
         public int ID { get; set; }
 
+        [Display(Name = "Post title")]
+        [DataType(DataType.Text)]
         public string   Title { get; set; }
 
+        [Display(Name = "Author name")]
+        [DataType(DataType.Text)]
         public string Author { get; set; }
 
+        [Display(Name = "Author website address")]
+        [DataType(DataType.Url)]
         public string AuthorSiteAddr { get; set; }
 
+        [Display(Name = "Post writing Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime PostDate { get; set; }
 
+        [Display(Name = "Post text")]
+        [DataType(DataType.MultilineText)]
         public string PostText { get; set; }
 
+        [Display(Name = "Post photo")]
+        [DataType(DataType.Text)]
         public string Photos { get; set; }
 
         public List<Comment> Comments { get; set; }
