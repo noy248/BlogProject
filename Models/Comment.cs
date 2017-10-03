6301c789e38eff52f211b7bfbde3ab69db05be54
noy248 <noya248@gmail.com>
2017-10-03 21:53:55 +0300

    Update Comment.cs
    adding the display names and types

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -18,16 +19,28 @@ namespace ShaulisBlog.Models
 
         public int ID { get; set; }
 
-        public string Title { get; set; }
-
         public int PostID { get; set; }
 
+
+        [Display(Name = "Commant title")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [Display(Name = "Commanter name")]
+        [DataType(DataType.Text)]
         public string Commenter { get; set; }
 
+        [Display(Name = "Commanter site address")]
+        [DataType(DataType.Url)]
         public string CommenterSiteAddr { get; set; }
 
+        [Display(Name = "Commant text")]
+        [DataType(DataType.MultilineText)]
         public string Text { get; set; }
 
+        [Display(Name = "Commant writing Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CommentDate { get; set; }
 
         Post CommentPost { get; set; }
