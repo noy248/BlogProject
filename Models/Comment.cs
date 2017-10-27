77571a4c6a752e701d9b7ea12d850ccc95805592
noy248 <noya248@gmail.com>
2017-10-27 17:11:04 +0300

    Update Comment.cs
    with the blog and managment

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -12,38 +13,42 @@ namespace ShaulisBlog.Models
 
         public Comment()
         {
-            this.ID = countIDs;
+            this.CommentID = countIDs;
             countIDs++;
         }
 
+        // [HiddenInput(DisplayValue = false)]
+        [ConcurrencyCheck]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CommentID { get; set; }
 
-        public int ID { get; set; }
-
+       //[ForeignKey("Post.PostID")]
         public int PostID { get; set; }
 
-
-        [Display(Name = "Commant title")]
+        [Display(Name = "Comment title")]
         [DataType(DataType.Text)]
         public string Title { get; set; }
 
-        [Display(Name = "Commanter name")]
+        [Display(Name = "Commenter name")]
         [DataType(DataType.Text)]
+        [StringLength(80, ErrorMessage = "Full name cannot be longer than 80 characters.")]
         public string Commenter { get; set; }
 
-        [Display(Name = "Commanter site address")]
+        [Display(Name = "Commenter site address")]
         [DataType(DataType.Url)]
         public string CommenterSiteAddr { get; set; }
 
-        [Display(Name = "Commant text")]
+        [Display(Name = "Comment text")]
         [DataType(DataType.MultilineText)]
         public string Text { get; set; }
 
-        [Display(Name = "Commant writing Date")]
+        [Display(Name = "Comment writing Date")]
         [DataType(DataType.DateTime)]
+        [ConcurrencyCheck]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CommentDate { get; set; }
 
-        Post CommentPost { get; set; }
+        public virtual Post CommentPost { get; set; }
 
     }
 }
